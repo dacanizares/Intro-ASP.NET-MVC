8d582217476d7ace9a40c359b167ecc6e822ebe1
Daniel Cañizares Corrales <dacanizares@outlook.com>
2016-08-21 20:54:32 -0500

    Add project files.

---
+++ ProjectDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectDemo.MvcApplication" Language="C#" %>
---
+++ ProjectDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectDemo.Startup))]
+namespace ProjectDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
