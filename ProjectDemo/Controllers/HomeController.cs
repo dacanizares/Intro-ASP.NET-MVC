01ab45bd025079ba21733bd06859a7dcb7ac1c26
Daniel Cañizares Corrales <dacanizares@outlook.com>
2016-09-06 01:34:28 -0500

    Lección 2

--- ProjectDemo/Controllers/HomeController.cs
+++ ProjectDemo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,27 @@ namespace ProjectDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult MostrarTodos()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var videojuegos = db.Videojuegos.ToList();
+            return PartialView("_MostrarTodos", videojuegos);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+        public ActionResult MostrarVideojuego(int id)
+        {           
+            Videojuego videojuego = db.Videojuegos.Find(id);
+            if (videojuego == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_MostrarVideojuego", videojuego);
         }
     }
 }
